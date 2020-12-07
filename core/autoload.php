263bbe583c074d90b95249044525c423f82ba5c0
otengkwame <developerkwame@gmail.com>
2020-12-07 17:29:22 +0000

    add: objectify function

--- core/autoload.php
+++ core/autoload.php
@@ -4,3 +4,20 @@
 require_once __DIR__ . "/../config.php";
 require_once "debug.php";
 
+/**
+ * Encode an array and retrieve as an object
+ *
+ * @param array $array
+ * @return void
+ */
+function objectify(array $array)
+{
+    if (is_array($array)) {
+        $array = json_encode($array);
+        return json_decode($array);
+    }
+
+    throw new Exception("Parameter must be array", 1);
+    
+    return false;
+}

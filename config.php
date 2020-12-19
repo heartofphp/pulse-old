a6d69dcd77c0e50758887847ff311c19fa52ac15
otengkwame <developerkwame@gmail.com>
2020-12-19 23:32:51 +0000

    :open_book: Doc: added documentation description

--- config.php
+++ config.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Pulse.
+ *
+ * (c) Kwame Oteng Appiah-Nti <me@developerkwame.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /**
  * Configuration for Pulse
  * Directory Structure and Path locations 

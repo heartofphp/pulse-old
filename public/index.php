e804aa87f5489280df9ee560d94c55b2d2fc20ec
otengkwame <developerkwame@gmail.com>
2021-01-21 23:51:49 +0000

    :ok_hand: Improve: add docs and autoload.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php 
 
+/*
+ * This file is part of Pulse.
+ *
+ * (c) Kwame Oteng Appiah-Nti <me@developerkwame.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__ . "/../core/autoload.php";
+
 /**
  * The index.php file for Pulse
  */

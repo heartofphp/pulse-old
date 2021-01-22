d4ebcd88cddf377aff42ff363dcfb7fc0c5a3b7a
otengkwame <developerkwame@gmail.com>
2021-01-22 23:59:36 +0000

    :ok_hand: Improve: add docs and arrays.php

--- helpers/arrays.php
+++ helpers/arrays.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of Pulse.
+ *
+ * (c) Kwame Oteng Appiah-Nti <me@developerkwame.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
 if ( ! function_exists('array_add'))
 {
 

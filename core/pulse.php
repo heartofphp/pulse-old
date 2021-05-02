d957b9eb73766b0b7725928cdd208d05056aeb03
otengkwame <developerkwame@gmail.com>
2021-05-02 23:58:47 +0000

    :sparkles: add pulse class

---
+++ core/pulse.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Pulse, a not so skeleton project for PHP beginners
+ * 
+ * For a heart beat there is a pulse 
+ * and that's what I (Pulse) stand for
+ * 
+ * This file is part of Pulse.
+ *
+ * Copyright (c) Kwame Oteng Appiah-Nti (Developer Kwame) <me@developerkwame.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * MIT License
+ */
+
+ class Pulse
+ {
+     
+ }
